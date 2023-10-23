5cf8b2b1ad48e5d60ab1c17a43e80ce367547375
Dera <deraegbo@gmail.com>
2023-10-23 21:41:05 +0100

    Made a change to UI. The anchor tag in EmailButton was given a style object {all: 'unset'}

--- src/components/EmailButton.js
+++ src/components/EmailButton.js
@@ -11,7 +11,7 @@ export default function EmailButton() {
     const mailtoLink = `mailto:${emailAddress}?subject=${subject}`;
 
     return (
-      <a href={mailtoLink}>
+      <a href={mailtoLink} style={ { all: 'unset' } }>
         <Button variant="contained" sx={{ marginTop: '30px', textTransform: 'capitalize', width: '350px', height: '70px', }}>
           <Typography variant="h6">Send us an email</Typography>
         </Button>
